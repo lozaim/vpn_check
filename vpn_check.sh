cd5f8dc9c5cc7b26e7d49f864815dcc77fe9db65
lozaim <65390783+lozaim@users.noreply.github.com>
2022-09-06 13:14:50 +0300

    Update vpn_check.sh

--- vpn_check.sh
+++ vpn_check.sh
@@ -2,10 +2,9 @@
 
 while [ 1 = 1 ]; do
 testip="192.168.25.1"
-
-testping=$(ping -c 1 $testip | grep "received" | awk '{print $4}')
-testcon=$(nmcli device | grep tun0 | awk '{print $3}')
-testwifi=$(nmcli device | grep Hotspot | awk '{print $3}')
+testping=$(ping -c 1 $testip | grep "received" | awk '{print $4}') # Check ping, 1 if exist
+testcon=$(nmcli device | grep tun0 | awk '{print $3}') # Check connection, connected is good
+testwifi=$(nmcli device | grep Hotspot | awk '{print $3}') # Check if Hotspot is enabled
 if [[ $testwifi == connected ]] &&  [[ $testcon != connected ]] && [[ $testping == 0 ]]
 	then
 	echo "NO PING,NO CONNECTION, disable hotspot" | logger -t NOPING
